9f78ee6ddb4c7fd265cafc224ff461df2f2965a7
Kanahiya <79072216+Kanahiya143@users.noreply.github.com>
2026-06-01 15:04:38 +0530

    function updated to script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const time = document.querySelector('#time')
+const today = document.querySelector('#date')
+const usTime = document.querySelector('#usTime')
 
 setInterval(function(){
   const date = new Date();
+  today.innerHTML = date.toLocaleDateString('en-GB')
   time.innerHTML = date.toLocaleTimeString();
+  usTime.innerHTML = date.toLocaleTimeString('en-US', {
+  timeZone: 'America/New_York'
+});
+
 },1000)
